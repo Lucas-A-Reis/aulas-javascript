0abd4966e4a46dbda5c577f8d946ee9fb7ac0951
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-07-30 16:40:01 -0300

    diferentes maneiras de colocar aspas no string

---
+++ js/meu-script.js
@@ -0,0 +1,17 @@
+let uc = 2;
+let competencia = "estilização em css e programação em java script";
+console.log(`Estamos na uc ${uc} estudando ${competencia}.`);
+
+//Oráculo
+
+const nomes = ["Vanderlei", "Kauan", "Edvanei", "Lucas", "William"];
+console.log(nomes[0]);
+console.log(nomes[4]);
+
+
+//Oráculo
+
+console.log("Gosto 'muito' de paçoca");
+console.log('Gosto "muito" de paçoca');
+console.log("Gosto \"muito\" de paçoca");
+console.log(`Gosto "muito" de paçoca`);
