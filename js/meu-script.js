53084ae00dcc1e5960114fef863aa615b410ece2
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-07-30 16:53:41 -0300

    modo estrito habilitado

--- js/meu-script.js
+++ js/meu-script.js
@@ -1,14 +1,13 @@
+'use strict';
+
 let uc = 2;
 let competencia = "estilização em css e programação em java script";
 console.log(`Estamos na uc ${uc} estudando ${competencia}.`);
 
 //Oráculo
-
 const nomes = ["Vanderlei", "Kauan", "Edvanei", "Lucas", "William"];
 console.log(nomes[0]);
 console.log(nomes[4]);
-
-
 //Oráculo
 
 console.log("Gosto 'muito' de paçoca");
