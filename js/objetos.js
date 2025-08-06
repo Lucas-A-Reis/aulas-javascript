52c2e28107eebcd985a7c3283a435836d2b70b8b
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-06 17:02:09 -0300

    faz array de objetos

--- js/objetos.js
+++ js/objetos.js
@@ -10,3 +10,21 @@ console.log(pessoa);
 
 console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos e mora em ${pessoa.cidade}.`);
 
+const livro = {
+    titulo: "O Senhor dos Anéis",
+    autor: "J.R.R. Tolkien",
+    volumes: ["A Sociedade do Anel", "As duas Torres", "O Retorno do Rei"]
+}
+
+console.log(livro);
+console.log(livro.autor);
+console.log(livro.volumes[1]);
+
+const livros = [
+    {titulo: "assassinato do expresso do oriente", autor: "agata christie"},
+    {titulo: "crepusculo", autor: "stephane meyers"},
+    {titulo: "ramayana", autor: "vyasa"}
+]
+;
+
+console.log(livros[2].autor);
