66a294dcf5391f521e29d0c06c14549d0e75efd3
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-06 14:34:18 -0300

    cria array heterogêneo (não é uma boa prática))

--- js/arrays.js
+++ js/arrays.js
@@ -3,3 +3,7 @@ let vocalistas = ["Ozzy","Dio","Floor Jansen", "Manoel Gomes"];
 console.log(vocalistas[0]);
 
 console.log(`O ${vocalistas[3]} está em turnê tocando PinkFloyd`)
+
+let idk = 6;
+const coisas = [10, "senac", "<h1>Oie</h1>", idk, 7.55];
+console.log(coisas[2]);
