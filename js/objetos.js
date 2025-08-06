05f9fee4e450c0551ea44c1e2340c9fad077dc0b
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-06 17:14:09 -0300

    exercício envolvendo objetos e arrays

--- js/objetos.js
+++ js/objetos.js
@@ -28,3 +28,30 @@ const livros = [
 ;
 
 console.log(livros[2].autor);
+
+/* Exercícios
+
+1. Crie um objeto chamado "aluno" contendo os seguintes dados:
+
+- Nome Completo
+- Data de Nascimento
+- (Use ARRAY) Lista de telefones (fixo e celular)
+- (DESAFIO: use um OBJETO nesta propriedade) Endereço contendo (separadamente):
+  - rua
+  - número
+  - bairro
+
+2. Mostre no console o nome do aluno, o telefone celular e o bairro em que mora. */
+
+let aluno = {
+    nome: "Cláudio Damaceno Yamaguchi",
+    data_de_nascimento: "08/08/1988",
+    telefones: ["99967-8881", "99988-8153"],
+    endereço: {
+        rua: "Rua Riacho",
+        número: 888,
+        bairro: "São Jorge",
+    } 
+}
+
+console.log(`o número do aluno ${aluno.nome} é ${aluno.telefones[1]} e ele reside no bairro ${aluno.endereço.bairro}`);
