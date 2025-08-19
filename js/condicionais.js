ccc511fb53a1206a9bbc37293ca3b0b14160c1ac
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-19 14:43:47 -0300

    cria variável e condicional

--- js/condicionais.js
+++ js/condicionais.js
@@ -8,3 +8,14 @@ if (estoque == true) {
 } else {
     console.log('estoque indisponível');
 }
+
+let aluno = {
+    nome: 'frodo bolseiro',
+    idade: 53
+};
+ 
+if (aluno.idade < 18) {
+    console.log(`${aluno.nome} é menor de idade`);
+} else {
+    console.log(`${aluno.nome} é maior de idade`);
+}
