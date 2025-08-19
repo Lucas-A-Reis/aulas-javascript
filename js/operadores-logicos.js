0fdbfea6e6047330c89fd466b7647e0f29dbfd38
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-19 16:53:57 -0300

    exemplifica e faz anotações sobre  o operador logico && e sobre a comparação de igualdade

--- js/operadores-logicos.js
+++ js/operadores-logicos.js
@@ -1,1 +1,25 @@
 'use script'
+/*
+    Operadores logicos
+    && -> e
+    || -> ou
+    ! -> não
+*/
+
+// && retorna true se todas as condições forem verdadeiras
+console.log("exemplo&&");
+let idade = 16;
+let temCarta = true;
+if (idade >= 18 && temCarta === true){
+    console.log('pode dirigir');
+} else {
+    console.log('não pode dirigir');
+}
+
+// sobre duplo e triplo sinal de ingualdade
+let a = 10;
+let b = '10';
+let resultado = a === b;
+console.log(resultado);
+// == compara apenas os caracteres e ignora a diferença de tipo de dado, se houver
+// === compara os caracteres e também o tipo de dado
