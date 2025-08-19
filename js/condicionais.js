257e8642167f4f6b944da1c998d1974e875bb583
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-19 16:03:58 -0300

    cria condicionais compostas com else/if

--- js/condicionais.js
+++ js/condicionais.js
@@ -2,7 +2,7 @@
 
 console.log('hahaha');
 let número = 50;
-let estoque =! false;
+let estoque = ! false;
 if (estoque == true) {
     console.log('estoque disponível');
 } else {
@@ -13,18 +13,29 @@ let aluno = {
     nome: 'frodo bolseiro',
     idade: 53
 };
- 
+
 if (aluno.idade < 18) {
     console.log(`${aluno.nome} é menor de idade`);
 } else {
     console.log(`${aluno.nome} é maior de idade`);
 };
 
-let nota = 0;
+let nota = 4;
 let nota2 = 10;
-let media = (nota+nota2)/2;
+let media = (nota + nota2) / 2;
 if (media >= 7) {
     console.log('aprovado!');
 } else {
     console.log('reprovado!');
 };
+
+if (media > 9) {
+    console.log('óptimo!');
+} else if(media > 7) {
+    console.log('excelente');
+} else if (media > 5) {
+    console.log('muito bem');
+} else {
+    console.log('desista!');
+};
+
