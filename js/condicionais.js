2afd80b885364ad5660cbf2e9dd28e7b3becc4a3
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-19 14:37:47 -0300

    cria variável e condicional

--- js/condicionais.js
+++ js/condicionais.js
@@ -1,3 +1,10 @@
 'use strict'
 
 console.log('hahaha');
+let número = 50;
+let estoque =! false;
+if (estoque == true) {
+    console.log('estoque disponível');
+} else {
+    console.log('estoque indisponível');
+}
