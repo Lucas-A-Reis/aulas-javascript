237c9d4d3518ae62dc6f27d6bed9520e144c342a
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-20 13:58:24 -0300

    faz uma anotação na qual é explicado o que é um loop

---
+++ js/loops.js
@@ -0,0 +1,8 @@
+'use strict'
+
+console.log("teste");
+
+/* loops são comandos que, baseados nalguma condição, executam uma ação repetidas vezes 
+Comandos mais tradicionais: while e for.
+Normalmente, os loops são controlados através de uma variável contadora.
+*/
