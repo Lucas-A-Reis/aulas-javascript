0f5b50bcc7810c996fdfd5a9c02f00ea7d13ef89
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-20 14:27:09 -0300

    faz um exemplo de loop com for

--- js/loops.js
+++ js/loops.js
@@ -2,17 +2,26 @@
 
 console.log("teste");
 
-/* loops são comandos que, baseados nalguma condição, executam uma ação repetidas vezes 
+/* 
+loops são comandos que, baseados nalguma condição, executam uma ação repetidas vezes 
 Comandos mais tradicionais: while e for.
-Normalmente, os loops são controlados através de uma variável contadora.
+Normalmente, os loops são controlados através de uma variável contadora (comumente chamada de "i", "j", "k" ou outra letra).
 */
 
 console.log("Exemplo 1: WHILE (enquanto)");
 
-let contador = 1;
-while (contador <= 5){
-    console.log("Valor do contador:"+contador);
-    contador++; // incremento 
-    // contador = contador + 1;
-    // contador += 1;
+let i = 1;
+while (i <= 5){
+    console.log("Valor do i:"+i);
+    i++; // incremento 
+    // i = i + 1;
+    // i += 1;
 }
+
+
+console.log("\nExemplo 2: FOR");
+
+// for(inicialização da variável, condição e atualização)
+for( let j = 1; j <= 10; j++ ){
+    console.log('J vale '+j);
+};
