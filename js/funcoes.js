cbdd0c4944bf5302f1ce1bf711d599b5b3ba340b
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-26 14:28:54 -0300

    aula-sobre-funcoes

---
+++ js/funcoes.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/*Sobre Funções
+Funções são blocos de código responsáveis por executar determinadas tarefas. Para usar estes blocos/funções, é necessário "chamar/invocar" a função desejada através do seu nome.
+ 
+Vantagens ao usar funções:
+- Reutilização de código
+- Organização de código-fonte, permitindo a separação de diferentes lógicas e responsabilidades
+- Performance melhor
+- Maior facilidade para manutenções
+ 
+No JavaScript, exitem varias maneiras de se criar uma função: função anônima, função nomeada/declarada e arrow function*/
+ 
+console.log("Exemplo 1: função anônima");
+ 
+const exemplo1 = function(){
+    //corpo da função: ação que função vai fazer
+    console.log("Ola Função Anônima");
+   
+}
+
+exemplo1();
+
+console.log('Exemplo 2: função nomeada (ou declarada)');
+
+ 
