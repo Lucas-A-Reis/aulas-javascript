8b683efe57cb5b165c5d13b436eacab45ea56a64
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-26 17:20:28 -0300

    usa o query selector

--- js/domeeventos.js
+++ js/domeeventos.js
@@ -1,1 +1,13 @@
-'use strict'
+'use strict';
+
+// Exemplo 1
+
+// Procurando (querySelector) por um elemento identificado por #exemplo01
+const exemplo01 = document.querySelector("#exemplo01");
+const mensagem01 = document.querySelector("#mensagem01");
+const pagina = document.querySelector("body");
+console.log(exemplo01);
+console.log(mensagem01);
+console.log(pagina);
+
+
