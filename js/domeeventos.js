3e629880090aaa5a0eb587671777b080359404eb
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-08-27 14:01:34 -0300

    querySelectorAll

--- js/domeeventos.js
+++ js/domeeventos.js
@@ -3,11 +3,13 @@
 // Exemplo 1
 
 // Procurando (querySelector) por um elemento identificado por #exemplo01
-const exemplo01 = document.querySelector("#exemplo01");
+const exemplo01 = document.getElementById("exemplo01");
 const mensagem01 = document.querySelector("#mensagem01");
 const pagina = document.querySelector("body");
 console.log(exemplo01);
 console.log(mensagem01);
 console.log(pagina);
-
+// Para selecionar todos os elementos, use querySelectorAll. O resultado será uma espécie de array chamada NodeList
+const paragrafo = document.querySelectorAll("p"); 
+console.log(paragrafo);
 
