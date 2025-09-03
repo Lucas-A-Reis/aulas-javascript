b49cf93cf799b56f93cd2c69ba026b70436cd88b
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-09-03 16:31:25 -0300

    cria campo de senha

--- js/campo-senha.js
+++ js/campo-senha.js
@@ -1,1 +1,19 @@
 'use strict'
+const campoSenha = document.querySelector("#senha");
+const botaoMostrar = document.querySelector("#mostrar");
+
+botaoMostrar.addEventListener("pointerdown", function () {
+    senha.type = "text";
+});
+
+botaoMostrar.addEventListener("pointerup", function () {
+    senha.type = "password";
+});
+
+// Pointerout, pointerleave, pointermove
+botaoMostrar.addEventListener("pointerout", function () {
+    senha.type = "password";
+});
+
+
+
