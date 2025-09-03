05f57d4b760456cfa92cd3b47248eab9cb6aa2da
Lucas-A-Reis <lucas.argoncalves@senacsp.edu.br>
2025-09-03 17:17:05 -0300

    anotações

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -2,10 +2,15 @@
 const campoBusca = document.querySelector("#busca");
 const clientes = document.querySelectorAll("#clientes li");
 
+//Monitora o input na barra de pesquisa
 campoBusca.addEventListener("input", function(){
+    //Constante "termo Digitato" igual ao valor inserido no "campoBusca"
     const termoDigitado = campoBusca.value.toLowerCase();
+    //Loop que verifica cada item de clientes para submetê-los a condição
     for (const cliente of clientes) {
+        //A condição que checa os itens da lista e compara com o input da pesquisa
         if(cliente.textContent.toLowerCase().includes(termoDigitado)){
+            //Muda display
             cliente.style.display = "list-item";
         } else {
             cliente.style.display = "none";
